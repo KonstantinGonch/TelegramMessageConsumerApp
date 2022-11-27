04606eb030570854ac88c75d5a1d37f93b57e63c
Goncharov Konstantin <goncharovkostya.1997@gmail.com>
2022-11-27 18:58:55 +0300

    Установка EF

---
+++ TelegramMessageConsumer/Middleware/ConsumerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramMessageConsumer.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace TelegramMessageConsumer.Middleware
+{
+	public class ConsumerContext : DbContext
+	{
+		public DbSet<Comment> Comments { get; set; }
+
+		public ConsumerContext()
+		{
+			//Database.EnsureCreated();
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Filename=Data\\appData.dat");
+		}
+	}
+}
---
+++ TelegramMessageConsumer/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramMessageConsumer.Models
+{
+	public  class Comment
+	{
+		public long Id { get; set; }
+		public string? UserName { get; set; }
+		public string? Content { get; set; }
+		public DateTime? PostedDate { get; set; }
+		public bool IsResponse { get; set; }
+	}
+}
